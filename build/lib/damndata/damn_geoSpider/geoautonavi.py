66c01ded38ae30ef6aedb4669cd208c02acac058
HUANG zixun <zixunhuang@outlook.com>
2021-04-01 18:49:59 +0800

    autonavi updated 0401

---
+++ build/lib/damndata/damn_geoSpider/geoautonavi.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   amapoiCollector_1.1.py
+@Time    :   2020/01/11 23:37:44
+@Author  :   Huang Zixun 
+@Version :   1.1
+@Contact :   zixunhuang@outlook.com
+'''
+from urllib import request
+import json
+import pandas as pd
+import csv,math
+import numpy as np
+from .GCJ02_to_WGS84 import GCJ2WGS
+
+def getpoi_page(poi_search_url, page):
+    req_url = 'http://'+poi_search_url+str(page)
+    data = ''
+    with request.urlopen(req_url) as f:
+        data = f.read()
+        data = data.decode('utf-8')
+    return data
+def hand(poilist, result):
+    pois = result['pois']
+    for i in range(len(pois)):
+        poilist.append(pois[i])
+def getpois(poi_search_url):
+    i = 1
+    poilist = []
+    while True:  
+        result = getpoi_page(poi_search_url, i)
+        print('Scraping...\n')
+        result = json.loads(result) 
+        if result.get('count','not exist') == 'not exist':
+            break
+        if result.get('count','not exist') == '0':
+            break
+        hand(poilist, result)
+        i = i + 1
+    return poilist
+def write_to_csv(poilist,filepath):
+    x=[]
+    for i in range(len(poilist)):
+        gcjlng=poilist[i]['location'].split(",")[0]
+        gcjlat=poilist[i]['location'].split(",")[1]
+        wgslng,wgslat=GCJ2WGS(gcjlng,gcjlat)
+        x.append([poilist[i]['id'],poilist[i]['name'],wgslng,wgslat, poilist[i]['adname'],poilist[i]['biz_ext'].get('cost','unknown'),poilist[i]['biz_ext'].get('rating','unknown'),poilist[i]['type']])
+    c=pd.DataFrame(x,columns=['id','name','wgslng','wgslat','adname','cost','rating','type'])
+    c.to_csv(filepath,encoding='utf-8')
+
---
+++ damndata/damn_geoSpider/GCJ02_to_WGS84.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import math
+import numpy as np
+
+def GCJ2WGS(lon, lat):
+    a = 6378245.0
+    ee = 0.00669342162296594323
+    PI = 3.14159265358979324
+    x = np.float64(lon) - 105.0
+    y = np.float64(lat) - 35.0
+    dLon = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * math.sqrt(abs(x))
+    dLon += (20.0 * math.sin(6.0 * x * PI) + 20.0 * math.sin(2.0 * x * PI)) * 2.0 / 3.0
+    dLon += (20.0 * math.sin(x * PI) + 40.0 * math.sin(x / 3.0 * PI)) * 2.0 / 3.0
+    dLon += (150.0 * math.sin(x / 12.0 * PI) + 300.0 * math.sin(x / 30.0 * PI)) * 2.0 / 3.0
+    dLat = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * math.sqrt(abs(x))
+    dLat += (20.0 * math.sin(6.0 * x * PI) + 20.0 * math.sin(2.0 * x * PI)) * 2.0 / 3.0
+    dLat += (20.0 * math.sin(y * PI) + 40.0 * math.sin(y / 3.0 * PI)) * 2.0 / 3.0
+    dLat += (160.0 * math.sin(y / 12.0 * PI) + 320 * math.sin(y * PI / 30.0)) * 2.0 / 3.0
+    radLat = np.float64(lat) / 180.0 * PI
+    magic = math.sin(radLat)
+    magic = 1 - ee * magic * magic
+    sqrtMagic = math.sqrt(magic)
+    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * PI)
+    dLon = (dLon * 180.0) / (a / sqrtMagic * math.cos(radLat) * PI)
+    wgsLon = np.float64(lon) - dLon
+    wgsLat = np.float64(lat) - dLat
+    return format(wgsLon,'.6f'),format(wgsLat,'.6f')
+
